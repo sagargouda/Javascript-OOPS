fa109720595fbc93decb2328e76405906404df90
sagar <sagarc2580@gmail.com>
2024-01-13 19:21:11 +0530

    getter and setter

---
+++ Setters/index.js
@@ -0,0 +1,17 @@
+// const account = {
+//   owner: "saar",
+//   movements: [200, 300, 300, 400, 5500],
+
+//   get latest() {
+//     return this.movements.slice(-1).pop();
+//   },
+
+//   set latest(mov) {
+//     this.movements.push(mov);
+//   },
+// };
+
+// console.log(account.latest);
+// account.latest = 50;
+
+////
